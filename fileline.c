0132efb73dea9fe1f792f71fe985f8b911e32fa4
Jan Beich <jbeich@FreeBSD.org>
2017-07-12 22:48:00 +0000

    * backtrace.c (getexecname): New function. 	(fileline_initialize): Prefer local `getexecname' on BSDs. 	* configure.ac: Detect sys/sysctl.h on BSDs. 	* configure: Regenerate. 	* config.h.in: Regenerate.

--- fileline.c
+++ fileline.c
@@ -39,8 +39,41 @@ POSSIBILITY OF SUCH DAMAGE.  */
 #include <stdlib.h>
 #include <unistd.h>
 
+#if defined(HAVE_SYS_SYSCTL_H)
+#include <sys/sysctl.h>
+#include <limits.h>
+#endif
+
 #include "backtrace.h"
 #include "internal.h"
+
+#if !defined(HAVE_GETEXECNAME) && defined(KERN_PROC_PATHNAME)
+/* Return pathname of executable or 0 on failure. */
+#define HAVE_GETEXECNAME
+static char execname[PATH_MAX + 1];
+static const char *
+getexecname (void)
+{
+  size_t path_len = sizeof(execname);
+  int mib[] = {
+    CTL_KERN,
+#if defined(__NetBSD__)
+    KERN_PROC_ARGS,
+    -1,
+    KERN_PROC_PATHNAME,
+#else
+    KERN_PROC,
+    KERN_PROC_PATHNAME,
+    -1,
+#endif
+  };
+
+  if (sysctl (mib, sizeof mib / sizeof mib[0], execname, &path_len, NULL, 0) < 0)
+    return NULL;
+
+  return execname;
+}
+#endif /* !HAVE_GETEXECNAME && KERN_PROC_PATHNAME */
 
 #ifndef HAVE_GETEXECNAME
 #define getexecname() NULL
