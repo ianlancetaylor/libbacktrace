68ddd6cf4d9f62038d3dec888e18642cd947b72d
Anton Bornev <a.bornev@bastion-tech.ru>
2020-04-11 04:34:10 +0300

    codestyle fixes

--- elf.c
+++ elf.c
@@ -2688,34 +2688,34 @@ backtrace_uncompress_zdebug (struct backtrace_state *state,
 /* Inline functions to access unaligned unsigned 32-bit integers */
 static inline uint32_t get_unaligned_le32(const uint8_t *buf)
 {
-	return (uint32_t)buf[0]
-			| ((uint32_t)buf[1] << 8)
-			| ((uint32_t)buf[2] << 16)
-			| ((uint32_t)buf[3] << 24);
+  return (uint32_t)buf[0]
+		  | ((uint32_t)buf[1] << 8)
+		  | ((uint32_t)buf[2] << 16)
+		  | ((uint32_t)buf[3] << 24);
 }
 
 static inline uint32_t get_unaligned_be32(const uint8_t *buf)
 {
-	return (uint32_t)(buf[0] << 24)
-			| ((uint32_t)buf[1] << 16)
-			| ((uint32_t)buf[2] << 8)
-			| (uint32_t)buf[3];
+  return (uint32_t)(buf[0] << 24)
+		  | ((uint32_t)buf[1] << 16)
+		  | ((uint32_t)buf[2] << 8)
+		  | (uint32_t)buf[3];
 }
 
 static inline void put_unaligned_le32(uint32_t val, uint8_t *buf)
 {
-	buf[0] = (uint8_t)val;
-	buf[1] = (uint8_t)(val >> 8);
-	buf[2] = (uint8_t)(val >> 16);
-	buf[3] = (uint8_t)(val >> 24);
+  buf[0] = (uint8_t)val;
+  buf[1] = (uint8_t)(val >> 8);
+  buf[2] = (uint8_t)(val >> 16);
+  buf[3] = (uint8_t)(val >> 24);
 }
 
 static inline void put_unaligned_be32(uint32_t val, uint8_t *buf)
 {
-	buf[0] = (uint8_t)(val >> 24);
-	buf[1] = (uint8_t)(val >> 16);
-	buf[2] = (uint8_t)(val >> 8);
-	buf[3] = (uint8_t)val;
+  buf[0] = (uint8_t)(val >> 24);
+  buf[1] = (uint8_t)(val >> 16);
+  buf[2] = (uint8_t)(val >> 8);
+  buf[3] = (uint8_t)val;
 }
 
 /*
